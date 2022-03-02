b5f1487f7c554363c8ced076ecb091ff82e3cd86
Jangilkyu <jang19.dev@gmail.com>
2022-03-02 23:33:51 +0900

    Add: tableView and TrackCell

---
+++ ITunesApp/ITunesApp/Cells/TrackCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCell.swift
+//  ITunesApp
+//
+//  Created by 장일규 on 2022/03/02.
+//
+
+import Foundation
+import UIKit
+
+class TrackCell: UITableViewCell{
+    
+}
--- ITunesApp/ITunesApp/Controller/SearchViewController.swift
+++ ITunesApp/ITunesApp/Controller/SearchViewController.swift
@@ -8,8 +8,70 @@
 import Foundation
 import UIKit
 
+let identifier = "TrackCell"
 class SearchViewController: UIViewController {
-    override func viewDidLoad() {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
         
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        setup()
+    }
+    
+    func setup() {
+        addViews()
+        setConstraints()
+        setConfigureTableView()
+    }
+    
+    
+    func addViews() {
+        view.addSubview(tableView)
+    }
+    
+    func setConstraints() {
+        tableViewConstraints()
+    }
+    
+    func setConfigureTableView() {
+        tableView.register(TrackCell.self, forCellReuseIdentifier: identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+    func tableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+
+}
+
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 3
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
