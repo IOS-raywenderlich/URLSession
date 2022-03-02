c2385b0f6dd654385613a96438545d5c3962ba97
Jangilkyu <jang19.dev@gmail.com>
2022-03-02 23:45:49 +0900

    Add: for SearchView NavigationController

---
+++ ITunesApp/ITunesApp/Controller/SearchNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchNavigationController.swift
+//  ITunesApp
+//
+//  Created by 장일규 on 2022/03/02.
+//
+
+import Foundation
+import UIKit
+
+
+class SearchNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        viewControllers = [SearchViewController()]
+    }
+}
+
+
+
