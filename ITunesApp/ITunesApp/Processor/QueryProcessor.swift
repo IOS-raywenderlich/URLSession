da2f723255a8df17870f27b0fcd3ed1a953afd1d
Jangilkyu <jang19.dev@gmail.com>
2022-03-03 23:50:31 +0900

    Update: Delegate QueryProcessor

--- ITunesApp/ITunesApp/Processor/QueryProcessor.swift
+++ ITunesApp/ITunesApp/Processor/QueryProcessor.swift
@@ -13,5 +13,30 @@ protocol QueryProcessorDelegate: AnyObject {
 }
 
 class QueryProcessor {
+    weak var delegate: QueryProcessorDelegate?
+    var errorMessage = ""
     
+    func fetchData(urlString: String, usage: ITunesURL) {
+        guard let url = URL(string: urlString) else { return }
+        
+        var reqeust = URLRequest(url: url)
+        reqeust.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) {
+            [weak self] data, response, error in
+            
+            guard error == nil,
+                  let httpResponse = (response as? HTTPURLResponse),
+                  httpResponse.statusCode == 200,
+                  
+                    let data = data else {
+                        self?.delegate?.didFailwith(error: error, response: response, usage: usage)
+                        return
+                    }
+            
+            self?.delegate?.didSucessWith(data: data, response: httpResponse, usage: usage)
+        }
+        dataTask.resume()
+    }
 }
